e8e54f6af232bfb06a74ad38570aede6738d139c
li932565422 <35482711+li932565422@users.noreply.github.com>
2021-07-27 09:15:28 +0800

    Add files via upload

---
+++ 西周“贾伯”带盖铜壶/js/ww.js
@@ -0,0 +1,25 @@
+$(window).load(function () {
+    // 文物旋转
+    document.onclick = function(){
+        controls.autoRotate = false
+    }
+    // 拆解按钮
+    var rotate = true
+    $('.ww-btn1').click(function () {
+        if (rotate) {
+            new TWEEN.Tween(mesh1.position)
+                .to({ y: 1 }, 1000).easing(TWEEN.Easing.Linear.None).start();
+            new TWEEN.Tween(mesh2.position)
+                .to({ y: -3 }, 1000).easing(TWEEN.Easing.Linear.None).start();
+           
+         
+            rotate = !rotate
+        } else {
+            new TWEEN.Tween(mesh1.position).to({  x:0,y: -2 }, 1000).easing(TWEEN.Easing.Linear.None).onComplete(function () {}).start()
+            new TWEEN.Tween(mesh2.position).to({  x:0,y: -2}, 1000).easing(TWEEN.Easing.Linear.None).onComplete(function () {}).start()
+           
+            rotate = !rotate
+        }
+    })
+
+})
